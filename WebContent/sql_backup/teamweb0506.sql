ba12541b438f7d00f24563a02d5e9e0251746ed4
regyo997 <64520100+regyo997@users.noreply.github.com>
2020-06-03 00:41:59 +0800

    sql更新

--- WebContent/sql_backup/teamweb0506.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `viewcount` (
-  `view_id` int(11) NOT NULL,
-  `view_time` datetime DEFAULT NULL,
-  `view_ip` varchar(100) CHARACTER SET utf8 DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
